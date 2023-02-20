c57c297b50d08cd5bd3e0d7e9b30c72a176739a0
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-20 18:03:51 +0900

    マイグレーションファイルの編集

--- db/migrate/20230220084609_devise_create_users.rb
+++ db/migrate/20230220084609_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false
+      t.string :second_name,        null: false
+      t.string :first_name_kana,    null: false
+      t.string :second_name_kana,   null: false
+      t.date :birth_day,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
