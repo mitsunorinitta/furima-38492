7d253b54812c6c752c52f815fd1fb2dfa6c888b0
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-23 19:59:46 +0900

    ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :second_name_kana, presence: true
+  validates :birth_day, presence: true
 end
