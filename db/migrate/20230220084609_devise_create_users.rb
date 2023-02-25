90c95a8593ea9d2eb3a838f17814e8b6fc95301b
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-25 16:21:20 +0900

    不要なコードの削除

--- db/migrate/20230220084609_devise_create_users.rb
+++ db/migrate/20230220084609_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       ## Database authenticatable
       t.string :nickname,           null: false
-      t.string :email,              null: false, unique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :first_name,         null: false
       t.string :second_name,        null: false
