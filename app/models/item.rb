d596242f51c12ab1c3785e59581814fb02a1b14a
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-26 17:51:50 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,15 @@
 class Item < ApplicationRecord
 
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :item_category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_ship_id, presence: true
+  validates :item_price, presence: true
+  validates 
+
   belongs_to :user
   has_one_attached :image
-  
 end
