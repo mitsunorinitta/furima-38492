a6c72cafe84babfd96bbbe08b10fd570adbec3ee
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-26 22:13:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
