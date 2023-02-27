c1281c0d4092b4d9508c0b3e9f86d9d58f5dbc66
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-27 21:57:06 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
 
   before_action :set_item, only: [:show, :edit, :update]
 
