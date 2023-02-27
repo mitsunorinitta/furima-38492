ad110044d71621c1e707d71bf9ba1b3a6284d932
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-27 19:42:19 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,8 @@ class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
   before_action :authenticate_user!, only: :new
 
+  before_action :set_item, only: :show
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,9 +21,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :shipping_charge_id, :prefecture_id, :days_ship_id, :item_price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
