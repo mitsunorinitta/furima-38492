95568625ec14271bbb15ac004deb643eb5d5222d
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-27 22:03:29 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
     if @item.user_id == current_user.id
-    else
       redirect_to root_path
     end
   end
