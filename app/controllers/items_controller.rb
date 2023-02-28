3c9ab8ad300247c11f8111faf129d399ab172991
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-02-28 20:32:03 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,11 +41,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
