bca868f43047494f0870ad4d38d9a14e1b6ef7e4
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-03-03 19:58:36 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,8 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../item_price");
+// require("../item_price");
+require("../card")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
