3027785eb3b80ac264e6c1f36d313617c3085866
mitsunorinitta <mitsunori.nitta1534@gmail.com>
2023-03-04 19:51:36 +0900

    Update order_addresses.rb

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    user_id { Faker::Number.non_zero_digit }
-    item_id { Faker::Number.non_zero_digit }
     post_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     municipalities { Faker::Address.city }
